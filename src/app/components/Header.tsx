2030fe086097f904a659ae10c6f813bbb3a47b3b
Exzur A Lat <ribf865HGCa@gmail.com>
2025-05-19 22:22:55 +0800

    fix: wrap Header component with ClerkProviderWrapper for context management

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,34 +1,35 @@
 'use client';
-
+import ClerkProviderWrapper from './ClerkProviderWrapper';
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="bg-black text-white px-10 py-2 flex items-center justify-between">
-      <Link href="/">
-        <Image
-          src="/assets/Logo.jpg"
-          alt="Minikaniko Logo"
-          width={150}
-          height={100}
-          className="object-contain"
-        />
-      </Link>
-      <div>
-        <SignedOut>
-          <SignInButton mode="modal">
-            <button className="bg-white text-black px-6 py-2 rounded hover:bg-gray-200">
-              Login
-            </button>
-          </SignInButton>
-        </SignedOut>
-
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
-      </div>
-    </header>
+    <ClerkProviderWrapper>
+      <header className="bg-black text-white px-10 py-2 flex items-center justify-between">
+        <Link href="/">
+          <Image
+            src="/assets/Logo.jpg"
+            alt="Minikaniko Logo"
+            width={150}
+            height={100}
+            className="object-contain"
+          />
+        </Link>
+        <div>
+          <SignedOut>
+            <SignInButton mode="modal">
+              <button className="bg-white text-black px-6 py-2 rounded hover:bg-gray-200">
+                Login
+              </button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </header>
+    </ClerkProviderWrapper>
   );
 }
