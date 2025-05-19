49d2694424b7023a20dfdbe2c34d4688045fe22a
Exzur A Lat <ribf865HGCa@gmail.com>
2025-05-19 09:04:05 +0800

    Add Footer component to the application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import Header from './components/Header';
 import { Geist, Geist_Mono } from 'next/font/google';
 import type { Metadata } from 'next';
+import Footer from './components/Footer';
 
 const geistSans = Geist({ variable: '--font-geist-sans', subsets: ['latin'] });
 const geistMono = Geist_Mono({ variable: '--font-geist-mono', subsets: ['latin'] });
@@ -17,9 +18,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}>
           <Header />
-          <main className="max-w-5xl mx-auto p-6">{children}</main>
+          <main>{children}</main>
+          <Footer />
         </body>
       </html>
     </ClerkProvider>
